e2d36515b29bf1630522c1655a27e4f32c7ebdd0
Marco Túlio Amaral <docs.mtamaral@gmail.com>
2023-11-22 21:27:56 -0300

    Translated code

---
+++ src/Ambiente.java
@@ -0,0 +1,86 @@
+import java.util.HashMap;
+
+/**
+ * Classe Ambiente - um ambiente em um jogo adventure.
+ *
+ * Esta classe eh parte da aplicacao "World of Zuul".
+ * "World of Zuul" eh um jogo de aventura muito simples, baseado em texto.
+ *
+ * Um "Ambiente" representa uma localizacao no cenario do jogo. Ele eh
+ * conectado aos outros ambientes atraves de saidas. As saidas sao
+ * nomeadas como norte, sul, leste e oeste. Para cada direcao, o ambiente
+ * guarda uma referencia para o ambiente vizinho, ou null se nao ha
+ * saida naquela direcao.
+ * 
+ * @author Michael Kölling and David J. Barnes (traduzido por Julio Cesar Alves)
+ * @version 2011.07.31 (2016.02.01)
+ */
+
+public class Ambiente {
+    private String descricao;
+    private HashMap<String, Ambiente> saidas; // armazena as saídas do ambiente
+
+    /**
+     * Cria um ambiente com a "descricao" passada. Inicialmente, ele
+     * nao tem saidas. "descricao" eh algo como "uma cozinha" ou "um jardim aberto".
+     * 
+     * @param descricao A descricao do ambiente.
+     */
+    public Ambiente(String descricao) {
+        this.descricao = descricao;
+        saidas = new HashMap<>();
+    }
+
+    /**
+     * Define a direção da saída do ambiente.
+     * 
+     * @param direcao A direção da saída.
+     * @param vizinho O ambiente para o qual a saída leva.
+     */
+    public void ajustarSaida(String direcao, Ambiente vizinho) {
+        saidas.put(direcao, vizinho);
+    }
+
+    /**
+     * @return A descrição curta do ambiente.
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * Retorna a descrição do ambiente na forma:
+     * Você está na entrada.
+     * Saídas: Sala1 Sala2 Sala3
+     * 
+     * @return Uma descrição longa do ambiente.
+     */
+    public String getDescricaoLonga() {
+        return "Você está " + descricao + ".\n" + direcoesDeSaida();
+    }
+
+    /**
+     * Retorna uma String descrevendo as saídas da sala.
+     * Saídas: Sala1 Sala2 Sala3
+     * 
+     * @return Detalhes das saídas do ambiente.
+     */
+    private String direcoesDeSaida() {
+        String textoSaidas = "";
+        for (String direcao : saidas.keySet()) {
+            textoSaidas = textoSaidas + direcao + " ";
+        }
+        return textoSaidas;
+    }
+
+    /**
+     * Retorna o ambiente que é alcançado se formos de um ambiente na
+     * direção "direcao". Se há um ambiente sem direção, é retornado nulo.
+     * 
+     * @param direcao A direção da saída.
+     * @return O ambiente presente na direção.
+     */
+    public Ambiente getSaida(String direcao) {
+        return saidas.get(direcao);
+    }
+}
---
+++ src/Analisador.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+/**
+ * Esta classe eh parte da aplicacao "World of Zuul".
+ * "World of Zuul" eh um jogo de aventura muito simples, baseado em texto.
+ * 
+ * Esse analisador le a entrada do usuario e tenta interpreta-la como um
+ * comando "Adventure". Cada vez que eh chamado ele le uma linha do terminal
+ * e tenta interpretar a linha como um comando de duas palavras. Ele retorna
+ * o comando como um objeto da classe Comando.
+ *
+ * O analisador tem um conjunto de palavras de comando conhecidas. Ele compara
+ * a entrada do usuario com os comandos conhecidos, e se a entrada nao eh um
+ * dos comandos conhecidos, ele retorna um objeto comando que eh marcado como
+ * um comando desconhecido.
+ * 
+ * @author Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ */
+public class Analisador {
+    private PalavrasComando comandos; // guarda todas as palavras de comando validas
+    private Scanner entrada; // origem da entrada de comandos
+
+    /**
+     * Cria um analisador para ler do terminal.
+     */
+    public Analisador() {
+        comandos = new PalavrasComando();
+        entrada = new Scanner(System.in);
+    }
+
+    /**
+     * @return O proximo comando do usuario
+     */
+    public Comando getComando() {
+
+        String inputLinha; // armazena uma linha inteira
+        String palavra1 = null;
+        String palavra2 = null;
+
+        System.out.print("> "); // imprime o prompt
+
+        inputLinha = entrada.nextLine();
+
+        // Tenta encontrar ate duas palavras na linha
+        Scanner tokenizer = new Scanner(inputLinha);
+        if (tokenizer.hasNext()) {
+            palavra1 = tokenizer.next(); // pega a primeira palavra
+            if (tokenizer.hasNext()) {
+                palavra2 = tokenizer.next(); // pega a segunda palavra
+                // obs: nós simplesmente ignoramos o resto da linha.
+            }
+        }
+
+        tokenizer.close();
+
+        // Agora verifica se esta palavra eh conhecida. Se for, cria um comando
+        // com ela. Se nao, cria um comando "null" (para comando desconhecido)
+        if (comandos.ehComando(palavra1)) {
+            return new Comando(palavra1, palavra2);
+        } else {
+            return new Comando(null, palavra2);
+        }
+
+    }
+
+    /**
+     * Imprime a lista de palavras de comandos válidas.
+     */
+    public void mostrarComandos() {
+        comandos.mostrarTodos();
+    }
+}
